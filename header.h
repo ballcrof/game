508e3f9af2b03e994e35be50fff96d60d9e71139
Brendan Allcroft <ballcrof@uoguelph.ca>
2020-01-13 12:51:18 -0500

    initial commit

---
+++ header.h
@@ -0,0 +1,4 @@
+int health(char* name);
+int damage(char* name);
+int speed(char* name);
+int compare(int first_cmp, int second_cmp);
---
+++ makefile
@@ -0,0 +1,5 @@
+game:	game.c
+	gcc -ansi -Wall -pedantic -o game.o game.c
+
+clean:
+	rm *.o
